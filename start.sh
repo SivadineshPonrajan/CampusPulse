0e04070cde1176d41f8cbb5e88e629f387a86974
Sivadinesh <31350867+SivadineshPonrajan@users.noreply.github.com>
2025-05-15 16:12:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+LOG_FILE="$SCRIPT_DIR/campuspulse.log"
+
+echo "===== Started at $(date) =====" >> "$LOG_FILE"
+
 source ~/.campuspulse/bin/activate
 echo "===== Starting run at $(date) ====="
-python /home/$USER/Desktop/campuspulse/main.py
+python /home/$(ls /home | head -n 1)/Desktop/campuspulse/main.py
