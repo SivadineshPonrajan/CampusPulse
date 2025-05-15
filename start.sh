1ecab9bb020ba884ef1e9568b9aaf389e78005fb
Sivadinesh <31350867+SivadineshPonrajan@users.noreply.github.com>
2025-05-15 15:59:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 source ~/.campuspulse/bin/activate
 echo "===== Starting run at $(date) ====="
-python /home/$(ls /home | head -n 1)/Desktop/campuspulse/main.py
+python /home/$USER/Desktop/campuspulse/main.py
