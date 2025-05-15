21a31137c26f741fc2461ceb2c8c0cd6c855e26e
Sivadinesh <31350867+SivadineshPonrajan@users.noreply.github.com>
2025-05-15 16:51:17 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-LOG_FILE="$SCRIPT_DIR/campuspulse.log"
+# SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+# LOG_FILE="$SCRIPT_DIR/campuspulse.log"
 
-echo "===== Started at $(date) =====" >> "$LOG_FILE"
+# echo "===== Started at $(date) =====" >> "$LOG_FILE"
+echo "===== Started at $(date) ====="
 
-# Try to connect for up to 5 seconds
+# Try to connect for up to 10 seconds
 SECONDS=0
 while ! ping -c 1 -W 1 8.8.8.8 &> /dev/null; do
-    if (( SECONDS >= 5 )); then
-        echo "No internet after 5 seconds, continuing at $(date)" >> "$LOG_FILE"
+    if (( SECONDS >= 10 )); then
+        echo "No internet after 10 seconds, continuing at $(date)"
         break
     fi
     sleep 1
 done
 
-echo "===== Started again at $(date) =====" >> "$LOG_FILE"
+echo "===== Started again at $(date) ====="
 
 source ~/.campuspulse/bin/activate
 cd /home/$(ls /home | head -n 1)/Desktop/campuspulse/
